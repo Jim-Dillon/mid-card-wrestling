6d25d86f90c8b5146414b63076d9cef68e0ef745
Jim-Dillon <james.dillon94@gmail.com>
2022-08-10 11:11:44 +0100

    top-trumps - make cards appear/disappear on draw

--- top-trumps/toptrumps.js
+++ top-trumps/toptrumps.js
@@ -1,24 +1,30 @@
 // winner will hold the value of output
 const output = document.querySelector('.winner');
 
+//logic to render info to screen
 const playButton = document.querySelector('.play-game');
 const nextButton = document.querySelector('.next-hand');
 const youImage = document.querySelector('.you-card-image');
 const computerImage = document.querySelector('.computer-card-image');
-
+const youName = document.querySelector('.you-card-name');
+const computerName = document.querySelector('.computer-card-name');
+const youOverall = document.querySelector('.you-card-ovr');
+const computerOverall = document.querySelector('.computer-card-ovr');
+const youCard = document.querySelector('.you-card-container');
+const computerCard = document.querySelector('.computer-card-container')
 
 // an array of wrestlers with their pertinent attributes
 wrestlers = [
-  {title:"'Hangman' Adam Page", ovr:99, img:"../Illustrations/game cards/hangman card.png"},
-  {title:"'Cleaner' Kenny Omega", ovr:98, img:"../Illustrations/game cards/kenny card.png"},
+  {title:"'Hangman' Page", ovr:99, img:"../Illustrations/game cards/hangman card.png"},
+  {title:" Kenny Omega", ovr:98, img:"../Illustrations/game cards/kenny card.png"},
   {title:"CM Punk", ovr:95, img:"../Illustrations/game cards/punk card.png"},
-  {title:"'American Dragon' Bryan Danielson", ovr:96, img:"../Illustrations/game cards/danielson card.png"},
+  {title:"Bryan Danielson", ovr:96, img:"../Illustrations/game cards/danielson card.png"},
   {title:"MJF", ovr:97, img:"../Illustrations/game cards/mjf card.png"},
-  {title:"'Absolute' Ricky Starks", ovr:94, img:"../Illustrations/game cards/starks card.png"},
+  {title:"Ricky Starks", ovr:94, img:"../Illustrations/game cards/starks card.png"},
   {title:"Britt Baker D.M.D.", ovr:96, img:"../Illustrations/game cards/britt card.png"},
   {title:"Jon Moxley", ovr:97, img:"../Illustrations/game cards/moxley card.png"},
   {title:"Jade Cargill", ovr:98, img:"../Illustrations/game cards/jade card.png"},
-  {title:"'Mad King' Eddie Kingston", ovr:93, img:"../Illustrations/game cards/kingston card.png"},
+  {title:"Eddie Kingston", ovr:93, img:"../Illustrations/game cards/kingston card.png"},
 ];
 
 
@@ -41,11 +47,14 @@ console.log(comp);
 
 // adds a src for images' 'img' property so they appear on screen
 
-youImage.src = user.img
+youImage.src = user.img;
+computerImage.src = comp.img;
 
+youName.innerText = user.title;
+computerName.innerText = comp.title;
 
-computerImage.src = comp.img
-
+youOverall.innerText = user.ovr;
+computerOverall.innerText = comp.ovr;
 
 // create function - establish all outcomes of fight. 
 const determineWinner = (user, comp) => {
@@ -67,12 +76,16 @@ playButton.addEventListener('click', e => {
     playButton.classList.add('grey-button');
     nextButton.classList.remove('grey-button');
     nextButton.classList.add('ready-button');
+    youCard.classList.remove('is-hidden');
+    computerCard.classList.remove('is-hidden');
 });   
 
 nextButton.addEventListener('click', e => {
     location.reload();
     playButton.classList.add('game-buttons');
     nextButton.classList.add('grey-button');
+    youCard.classList.add('is-hidden');
+    computerCard.classList.add('is-hidden');
 })
 
 
